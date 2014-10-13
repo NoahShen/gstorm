52cfeef39a476f4206a3b7f597f764096b7507d1
noahshen <Noah.Shen87@gmail.com>
2014-10-13 22:30:52 +0800

    table annotation

--- src/main/groovy/gstorm/annotation/Table.groovy
+++ src/main/groovy/gstorm/annotation/Table.groovy
@@ -5,5 +5,7 @@ import java.lang.annotation.Retention
 import static java.lang.annotation.RetentionPolicy.RUNTIME
 
 @Retention(RUNTIME)
-@interface Entity {
+@interface Table {
+
+    String name()
 }
